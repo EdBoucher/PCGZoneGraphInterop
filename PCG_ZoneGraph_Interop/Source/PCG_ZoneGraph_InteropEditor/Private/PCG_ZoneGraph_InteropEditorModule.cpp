653e4779ea3e9c3f332398147de320023d0ecd8d
mikec316 <mike.chapman316@gmail.com>
2025-02-23 13:15:35 +0000

    Fix ZoneShape missing

---
+++ PCG_ZoneGraph_Interop/Source/PCG_ZoneGraph_InteropEditor/Private/PCG_ZoneGraph_InteropEditorModule.cpp
@@ -0,0 +1,18 @@
+﻿#include "PCG_ZoneGraph_InteropEditorModule.h"
+
+#define LOCTEXT_NAMESPACE "FPCG_ZoneGraph_InteropEditorModule"
+
+void PCG_ZoneGraph_InteropEditorModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void PCG_ZoneGraph_InteropEditorModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(PCG_ZoneGraph_InteropEditorModule, PCG_ZoneGraph_InteropEditor)
---
+++ PCG_ZoneGraph_Interop/Source/PCG_ZoneGraph_InteropEditor/Public/PCG_ZoneGraph_InteropEditorModule.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+class PCG_ZoneGraph_InteropEditorModule : public IModuleInterface
+{
+public:
+	/** IModuleInterface implementation */
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
